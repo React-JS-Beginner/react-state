c362965f215c2f1ca7a7b2bdb3be5db594b1f19d
avishekataur <avishekataur@gmail.com>
2021-09-19 10:29:03 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <Counter></Counter>
+    </div>
+  );
+}
+
+function Counter() {
+  /* 
+  //Multiple Line
+  const [count, setCount] = useState(0);
+
+  const increaser = () =>
+  {
+    const increaseCount = count + 1;
+    return setCount(increaseCount)
+  }
+
+  const decreaser = () =>
+  {
+    const decreaseCount = count - 1;
+    return setCount(decreaseCount)
+  }
+ */
+
+  //Single Line
+  const [count, setCount] = useState(0);
+
+  const increaser = () => setCount(count + 1);
+
+  const decreaser = () => setCount(count - 1);
+
+  const resetCounter = () => setCount(count * 0);
+  
+  return (
+    <div className="counter">
+      <div className="circle">
+        <span>{count}</span>
+      </div>
+
+      <div className="buttons">
+        <button onClick={increaser}>
+          <span>+</span>
+        </button>
+
+        <button onClick={decreaser}>
+          <span>-</span>
+        </button>
+
+        <button className="reset" onClick={resetCounter}>
+          <span>RESET</span>
+        </button>
+      </div>
     </div>
   );
 }
